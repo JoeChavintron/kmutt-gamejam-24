b75659acc91f7401c1af7a5c9f989aa8215ce990
Prayoch rujira <prayoch@clockupstudio.com>
2024-01-27 15:01:18 +0700

    User RigidBody's velocity instead of move transform directly

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -5,12 +6,18 @@ public class Movement : MonoBehaviour
     private bool _isMoving;
     private Vector2 _direction;
 
-    public float speed = .01f;
+    public float speed = 100f;
+
+    public Rigidbody body;
     
     private void Update()
     {
-        if (!_isMoving) return;
-        transform.position = new Vector3(transform.position.x + _direction.x * speed, transform.position.y, transform.position.z + _direction.y * speed);
+        if (!_isMoving)
+        {
+            body.velocity = Vector3.zero;
+            return;
+        }
+        body.velocity = new Vector3(_direction.x*speed, 0, _direction.y*speed);
     }
 
     public void OnMove(Vector2 direction)
